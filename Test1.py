00c042d0b873b8711ed4d22e43e436b97d6bc9e2
David <davidpasincev8@gmail.com>
2023-11-15 19:14:15 +0300

    Мой первый комид

---
+++ Test1.py
@@ -0,0 +1,12 @@
+import requests
+import cmath
+q1 = input('Столица России?')
+print(q1)
+q2 = input('Столица Чехии?')
+print(q2)
+q3 = input('Столица Америки?')
+print(q3)
+q4 = input('Столица Франции?')
+print(q4)
+q5 = input('Столица Африки?')
+print(q5)
